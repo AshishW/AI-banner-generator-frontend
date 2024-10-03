8fdec1e3608f6a3107a9b84b2486647fce1f97db
AshishW <aswaikar123@gmail.com>
2024-10-03 15:28:43 +0530

    updating config file

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/AI-banner-generator-frontend/', // important for GitHub Pages
-  build: {
-    outDir: 'dist', // Default output folder, can be changed
-  },
+  plugins: [react()]
+  // base: '/AI-banner-generator-frontend/', // important for GitHub Pages
+  // build: {
+  //   outDir: 'dist', // Default output folder, can be changed
+  // },
 });
